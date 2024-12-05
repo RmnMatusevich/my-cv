96539b77177b99e91be12f66f8ae1405187cce4e
MacBook <macbook@macbooks-mbp.home>
2024-12-05 21:01:23 +0100

    feat: push to git

---
+++ src/data/resume-data.tsx
@@ -0,0 +1,334 @@
+import {
+  ClevertechLogo,
+  ConsultlyLogo,
+  JojoMobileLogo,
+  MonitoLogo,
+  NSNLogo,
+  ParabolLogo,
+} from "@/images/logos";
+import { GitHubIcon, LinkedInIcon } from "@/components/icons";
+
+export const RESUME_DATA = {
+  name: "Raman Matusevich",
+  initials: "RM",
+  location: "Warsaw, Poland, CET",
+  locationLink: "https://www.google.com/maps/place/Warsaw",
+  about:
+    "Experienced Software Engineer passionate about crafting robust, scalable, and high-quality software solutions.",
+  summary: (
+    <>
+      Experienced Senior Software Engineer with a strong focus on creating
+      reliable, efficient, and high-quality software solutions. I’m passionate
+      about delivering impactful products by cosmbining technical expertise with
+      a commitment to excellence in every stage of development.
+    </>
+  ),
+  avatarUrl:
+    "https://media.licdn.com/dms/image/v2/C4D03AQHZizgvGDTEDA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1668096966064?e=1738800000&v=beta&t=mb64AtqNSd0o3adhRBd--YdcvjXyMOnYPHHqaBIrzbA",
+  personalWebsiteUrl: "",
+  contact: {
+    email: "rmnmatusevich@gmail.com",
+    tel: "+48786406643",
+    social: [
+      {
+        name: "GitHub",
+        url: "https://github.com/RmnMatusevich",
+        icon: GitHubIcon,
+      },
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/roman-matusevich-7650bb184/",
+        icon: LinkedInIcon,
+      },
+    ],
+  },
+  education: [
+    {
+      school: "Belarusian State University of Informatics and Radioelectronics",
+      degree: "Bachelor's Degree in Computer Science and E-business",
+      start: "2018",
+      end: "2022",
+    },
+  ],
+  work: [
+    {
+      company: "Volvo",
+      link: "https://film.io",
+      badges: [
+        "On Site",
+        "React",
+        "Nest.js",
+        "TypeScript",
+        "Node.js",
+        "Azure",
+        "PosgreSQL",
+        "3rd parties integrations",
+      ],
+      title: "Senior Software Engineer",
+      logo: ConsultlyLogo,
+      start: "2022",
+      end: "2024",
+      description: (
+        <>
+          Leading frontend development using React to build maintanable solution
+          using best possible UX/UI principles.
+          <ul className="list-inside list-disc">
+            <li>Frontend Architecture setup and technology stack creation</li>
+            <li>
+              Established release process enabling faster deployments and
+              reliable rollbacks
+            </li>
+            <li>Implementing monitoring and security improvements</li>
+          </ul>
+          Perform backend development using Nest.js to build scalable and
+          integrationable solution.
+          <ul className="list-inside list-disc">
+            <li>
+              Building API with Nest.js that provision REST, GraphQL, as well as
+              Event-Driven solution with Azure communication channels
+            </li>
+            <li>Identify high-level and low-level architecture</li>
+            <li>Integrations with 3rd party services</li>
+            <li>Backoffice development</li>
+          </ul>
+          Being a part of recruitment proccess to involve professionals in
+          company.
+        </>
+      ),
+    },
+    {
+      company: "Pletly",
+      link: "https://pletly.com",
+      badges: [
+        "Remote",
+        "React Native",
+        "Expo",
+        "TypeScript",
+        "Nest.js",
+        "GraphQL",
+        "Tailwind",
+      ],
+      title: "Senior Software Engineer",
+      logo: ParabolLogo,
+      start: "2021",
+      end: "2022",
+      description: (
+        <>
+          Developing full stack mobile application. Specialized for people with
+          disabilities.
+          <ul className="list-inside list-disc">
+            <li>Built mobile application with React Native and Expo</li>
+            <li>Implementing multi-tenant chat functionality</li>
+            <li>Developing GraphQL apis</li>
+            <li>Developing multi-step account creation and verification</li>
+          </ul>
+        </>
+      ),
+    },
+    {
+      company: "Upwork",
+      link: "https://www.upwork.com/freelancers/~01a33fed7f8a68406e?mp_source=share",
+      badges: [
+        "Remote",
+        "React",
+        "Next.js",
+        "TypeScript",
+        "Node.js",
+        "Nest.js",
+        "Express.js",
+        "React Native",
+        "Stripe",
+        "Ecommerce",
+        "Firebase",
+        "PosgreSQL",
+        "MongoDB",
+        "Redis",
+        "AWS",
+        "Expo",
+        "web3",
+      ],
+      title: "Software Engineer",
+      logo: ClevertechLogo,
+      start: "2021",
+      end: "2022",
+      description: (
+        <>
+          Developing application and consult companies to achive goals,
+          requirements management, high- and low-level architecture.
+          <ul className="list-inside list-disc">
+            <li>
+              Search Platform for a Music App: Developed with React and
+              TypeScript for real-time music discovery
+            </li>
+            <li>
+              Anonymous Gift App: Built mobile app using React Native to support
+              anonymous gifting.
+            </li>
+            <li>
+              Parcel Delivery App: Created a web solution integrating Shippo for
+              logistics and Stripe for payments.
+            </li>
+            <li>
+              Real Estate App: Developed a mobile app for property management
+              using React Native and Express.
+            </li>
+            <li>
+              NFT Marketplace for Game: Integrated The Graph and TypeScript for
+              seamless transactions
+            </li>
+            <li>
+              Event Booking Platform: Built a comprehensive system with MongoDB,
+              Stripe, and GiftUp for payments and subscriptions
+            </li>
+          </ul>
+        </>
+      ),
+    },
+    {
+      company: "HF-Solution LLC",
+      link: "https://hf-solution.com",
+      badges: [
+        "On Site",
+        "React Native",
+        "Express.js",
+        "React",
+        "web3",
+        "blockchain",
+      ],
+      title: "Software Engineer",
+      logo: JojoMobileLogo,
+      start: "2020",
+      end: "2021",
+      description: (
+        <>
+          <ul className="list-inside list-disc">
+            <li>
+              Social Networking App: Designed a React Native application to
+              foster user connections.
+            </li>
+            <li>
+              IT Communications Startup: Contributed to an innovative
+              communication platform for businesses.
+            </li>
+            <li>
+              Crypto Web Wallet: Developed a crypto wallet supporting rUSDT,
+              rETH, rBTC, and other tokens. Features included Transfer, Deposit,
+              Investment, Staking, and Pooling, leveraging Polkadot and Ethereum
+              APIs.
+            </li>
+          </ul>
+        </>
+      ),
+    },
+    {
+      company: "Fresh Lime Soft",
+      link: "https://www.linkedin.com/company/fresh-lime-software-llc/?trk=public_profile_experience-item_profile-section-card_image-click&originalSubdomain=pl",
+      badges: [
+        "On Site",
+        "React",
+        "Express.js",
+        "AWS",
+        "Stripe",
+        "Vimeo Streaming",
+        "Socket.io",
+        "MongoDB",
+        "PosgreSQL",
+        "MySql",
+        "Docker",
+        "JQuery",
+      ],
+      title: "Junior Software Engineer",
+      logo: NSNLogo,
+      start: "2019",
+      end: "2020",
+      description: (
+        <>
+          Here is where career starts...
+          <ul className="list-inside list-disc">
+            <li>Database Architecutre Design.</li>
+            <li>Building reusable and adaptive components with React.</li>
+            <li>
+              Creation of storage for video files shared on Training Portal.
+            </li>
+            <li>
+              Setting up deployement strategies and manage deployement ecosystem
+              with AWS. Partially being as DevOps specialist.
+            </li>
+            <li>
+              Taking part in creation of single-tenant database for multi-tenant
+              application, where each database hosted for newly registered user.
+            </li>
+            <li>
+              Developing secured chat channels with data compressing and
+              decompressing, encryption and chunking.
+            </li>
+          </ul>
+        </>
+      ),
+    },
+  ],
+  skills: [
+    "React/Next.js/Remix",
+    "TypeScript",
+    "Tailwind CSS/MUI/Ant/Custom design systems",
+    "Figma",
+    "WebSockets",
+    "Node.js",
+    "Nest.js",
+    "GraphQL",
+    "Angular",
+    "Message Brokers",
+    "MongoDB",
+    "PosgreSQL",
+    "Firebase",
+    "Supabase",
+    "Azure",
+    "AWS",
+    "System Architecture",
+    "Business prototyping",
+  ],
+  projects: [
+    {
+      title: "Monito",
+      techStack: ["TypeScript", "Next.js", "Browser Extension", "PostgreSQL"],
+      description:
+        "Browser extension for debugging web applications. Includes taking screenshots, screen recording, E2E tests generation and generating bug reports",
+      logo: MonitoLogo,
+      link: {
+        label: "monito.dev",
+        href: "https://monito.dev/",
+      },
+    },
+    {
+      title: "Consultly",
+      techStack: [
+        "TypeScript",
+        "Next.js",
+        "Vite",
+        "GraphQL",
+        "WebRTC",
+        "Tailwind CSS",
+        "PostgreSQL",
+        "Redis",
+      ],
+      description:
+        "Platform for online consultations with real-time video meetings and scheduling",
+      logo: ConsultlyLogo,
+      link: {
+        label: "consultly.com",
+        href: "https://consultly.com/",
+      },
+    },
+    {
+      title: "Minimalist CV",
+      techStack: ["TypeScript", "Next.js", "Tailwind CSS"],
+      description:
+        "An open source minimalist, print friendly CV template with a focus on readability and clean design. >9k stars on GitHub",
+      logo: MonitoLogo,
+      link: {
+        label: "Minimalist CV",
+        href: "https://github.com/BartoszJarocki/cv",
+      },
+    },
+  ],
+} as const;
